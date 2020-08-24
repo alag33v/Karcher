7676821498ed2615f4ac633b7dd89a2b703d404f
alageev <alageev5610@gmail.com>
2020-08-24 14:44:58 +0300

    Added adaptability to the site

--- js/app.js
+++ js/app.js
@@ -13,23 +13,62 @@ $(document).ready(function () {
     dots: true,
     slidesToShow: 5,
     asNavFor: '.reviews__slider-text',
+    responsive: [
+      {
+        breakpoint: 1151,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 501,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
   });
 
   // Animation
   new WOW().init();
 
-    // Smooth scroll
-    $('[data-scroll]').on('click', function (evt) {
+  // Smooth scroll
+  $('[data-scroll]').on('click', function (evt) {
+    evt.preventDefault();
+
+    const elementClass = $(this).data('scroll');
+    const elementOffset = $(elementClass).offset().top;
+
+    $('html, body').animate(
+      {
+        scrollTop: elementOffset,
+      },
+      1000
+    );
+  });
+  // Burger menu
+  const wrapper = document.querySelector('.burger__button-wrapper');
+  const burger = document.querySelector('.burger__button');
+  const mobileNav = document.querySelector('.menu__mobile-nav');
+  const mobileLink = document.querySelectorAll('.mobile-nav__link');
+  const html = document.querySelector('html');
+  const body = document.querySelector('body');
+
+  wrapper.addEventListener('click', (e) => {
+    e.preventDefault();
+    burger.classList.toggle('active');
+    mobileNav.classList.toggle('menu__mobile-nav--active');
+    html.classList.toggle('block');
+    body.classList.toggle('block');
+  });
+
+  for (let i = 0; i < mobileLink.length; i++) {
+    mobileLink[i].addEventListener('click', (evt) => {
       evt.preventDefault();
-  
-      const elementClass = $(this).data('scroll');
-      const elementOffset = $(elementClass).offset().top;
-  
-      $('html, body').animate(
-        {
-          scrollTop: elementOffset,
-        },
-        1000
-      );
+      burger.classList.toggle('active');
+      mobileNav.classList.remove('menu__mobile-nav--active');
+      html.classList.remove('block');
+      body.classList.remove('block');
     });
+  }
 });
